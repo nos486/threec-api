eea59f099a38b2c6cbe9458ce6f708a84262ffbe
ccnos <g8p$6mxHYD3$d%y>
2021-03-30 00:15:17 +0430

    update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require('./app');
 const db = require("./db")
 const socket =require("./socket")
 
-const port = 3000
+const port = 80
 
 const server = app.listen(port, () => {
     console.log(`Example app listening at http://0.0.0.0:${port}`)
